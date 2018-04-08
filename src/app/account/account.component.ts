fa2162827224be644fa24bf5e14a61854a9eab5c
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-08 15:28:02 +0200

    cross component communication using services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,18 +1,16 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {LoggingService} from '../logging.service';
 import {AccountsService} from '../accounts.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  // providers: [LoggingService]
 })
 export class AccountComponent implements OnInit {
   @Input() account: { name: string, status: string };
   @Input() id: number;
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+  constructor(private accountsService: AccountsService) {
   }
 
   ngOnInit() {
@@ -20,6 +18,6 @@ export class AccountComponent implements OnInit {
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id, status);
-    // this.loggingService.logStatusChange(status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,16 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import {LoggingService} from '../logging.service';
 import {AccountsService} from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  // providers: [LoggingService]
 })
 export class NewAccountComponent implements OnInit {
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+  constructor(private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status));
   }
 
   ngOnInit() {
@@ -18,6 +18,5 @@ export class NewAccountComponent implements OnInit {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    // this.loggingService.logStatusChange(accountStatus);
   }
 }
