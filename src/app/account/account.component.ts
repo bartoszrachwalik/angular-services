9c0832ec8c0957d958d3189fabc81e8684a6fc80
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-08 14:03:37 +0200

    project setup

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  @Input() account: { name: string, status: string };
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSetTo(status: string) {
+    this.statusChanged.emit({id: this.id, newStatus: status});
+    console.log('A server status changed, new status: ' + status);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { NewAccountComponent } from './new-account/new-account.component';
-
+import {AppComponent} from './app.component';
+import {AccountComponent} from './account/account.component';
+import {NewAccountComponent} from './new-account/new-account.component';
 
 @NgModule({
   declarations: [
@@ -19,4 +17,5 @@ import { NewAccountComponent } from './new-account/new-account.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
