e6d54b4f135c42373a3203af4d68809d439dd41a
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-04-08 14:30:43 +0200

    adds login service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,16 +1,18 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent implements OnInit {
   @Input() account: { name: string, status: string };
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
 
-  constructor() {
+  constructor(private loggingService: LoggingService) {
   }
 
   ngOnInit() {
@@ -18,6 +20,6 @@ export class AccountComponent implements OnInit {
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
   }
 }
